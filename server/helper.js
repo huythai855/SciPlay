1d1b31af5327340886bdbef0c3bdfb00c754e427
np4s <npanh1104@gmail.com>
2023-08-04 19:43:21 +0700

    feat: request with user_id, add thread and ranking

--- server/helper.js
+++ server/helper.js
@@ -3,9 +3,8 @@ const { insertDataToBigQuery } = require('./big_query/insert.js');
 const { updateDataInBigQuery } = require('./big_query/update.js');
 const { bigqueryClient } = require('./big_query_client.js');
 
-const user_id = '1';
 // Student informations
-async function getStudent() {
+async function getStudent(user_id) {
     const [rows] = await getDataFromBigQuery(bigqueryClient, {
         datasetId: 'sciplay_dataset',
         tableId: 'student_information',
@@ -28,6 +27,7 @@ async function getStudent() {
     return student;
 }
 
+///COURSE
 async function getCourse(course_id){
     const [rows] = await getDataFromBigQuery(bigqueryClient, {
         datasetId: 'sciplay_dataset',
@@ -39,21 +39,8 @@ async function getCourse(course_id){
     rows.course_id = course_id;
     return rows;
 }
-
-async function getLesson(lesson_id) {
-    const [rows] = await getDataFromBigQuery(bigqueryClient, {
-        datasetId: 'sciplay_dataset',
-        tableId: 'lesson',
-        column: ['name', 'course_id', 'lesson_id', 'duration', 'type', 'link', 'content'],
-        conditions: [`lesson_id = ${lesson_id}`],
-        limit: 1,
-    });
-
-    return rows;
-}
-
 // Student's courses
-async function getStudentCourses() {
+async function getStudentCourses(user_id) {
     const rows = await getDataFromBigQuery(bigqueryClient, {
         datasetId: 'sciplay_dataset',
         tableId: 'course_attendance',
@@ -69,31 +56,8 @@ async function getStudentCourses() {
     }
     return rows;
 }
-
-// Student's lesson
-async function getStudentLessons() {
-    let rows = await getDataFromBigQuery(bigqueryClient, {
-        datasetId: 'sciplay_dataset',
-        tableId: 'lesson_attendance',
-        column: ['lesson_id'],
-        conditions: [`student_id=${user_id}`],
-        limit: 3,
-    });
-    // console.log(rows);
-
-    // rows = [{lesson_id : '1'}, {lesson_id : '2'}, {lesson_id : '3'}, {lesson_id : '4'}]
-    for (let i = 0; i < rows.length; i++) {
-        const { lesson_id } = rows[i];
-        const lesson = await getLesson(lesson_id);
-        rows[i] = lesson;
-    }
-
-    // console.log(rows);
-    return rows;
-}
-
 // Student's finished courses
-async function getFinishedCourses() {
+async function getFinishedCourses(user_id) {
     const date = new Date().toJSON().slice(0, 10);
     const rows = await getDataFromBigQuery(bigqueryClient, {
         datasetId: 'sciplay_dataset',
@@ -110,13 +74,146 @@ async function getFinishedCourses() {
     }
     return rows;
 }
+// Specific course' topics
+async function getInsideCourse(course_id) {
+    const query = 'SELECT * FROM `sciplay_dataset.course_path` WHERE course_id = ' + course_id + ' ORDER BY id ASC';
+    const options = {
+        query: query,
+        location: 'US',
+    }
 
+    const [rows] = await bigqueryClient.query(options);
+    return rows;
+}
 
-// async function Test () {
-//     const stu = await getStudent();
-//     console.log(stu);
-// }
-// Test();
+///LESSON
+async function getLesson(lesson_id) {
+    const [rows] = await getDataFromBigQuery(bigqueryClient, {
+        datasetId: 'sciplay_dataset',
+        tableId: 'lesson',
+        column: ['*'],
+        conditions: [`lesson_id = ${lesson_id}`],
+        limit: 1,
+    });
+    rows.content = JSON.parse(rows.content);
+
+    return rows;
+}
+
+async function getLessonName(lesson_id) {
+    const [rows] = await getDataFromBigQuery(bigqueryClient, {
+        datasetId: 'sciplay_dataset',
+        tableId: 'lesson',
+        column: ['name', 'lesson_id'],
+        conditions: [`lesson_id = ${lesson_id}`],
+        limit: 1,
+    });
+
+    return rows;
+}
+// Student's lesson
+async function getStudentLessons(user_id) {
+    let rows = await getDataFromBigQuery(bigqueryClient, {
+        datasetId: 'sciplay_dataset',
+        tableId: 'lesson_attendance',
+        column: ['lesson_id'],
+        conditions: [`student_id=${user_id}`],
+        limit: 3,
+    });
+    // console.log(rows);
+
+    // rows = [{lesson_id : '1'}, {lesson_id : '2'}, {lesson_id : '3'}, {lesson_id : '4'}]
+    for (let i = 0; i < rows.length; i++) {
+        const { lesson_id } = rows[i];
+        const lesson = await getLessonName(lesson_id);
+        rows[i] = lesson;
+    }
+
+    // console.log(rows);
+    return rows;
+}
+// Lesson inside a topic
+async function getTopicLesson(course_path_id){
+    const query = 'SELECT lesson_id FROM `sciplay_dataset.lesson` WHERE course_path_id = ' + course_path_id + ' ORDER BY lesson_id ASC';
+    const options = {
+        query: query,
+        location: 'US',
+    }
+
+    const [rows] = await bigqueryClient.query(options);
+    return rows;
+}
+
+///RANK
+// Rank in descending
+async function getRanking(order, limit) {
+    const query = 'SELECT name, total_stars, current_level FROM `sciplay_dataset.student_information` ORDER BY ' + order + ' DESC LIMIT ' + limit + ';';
+    const options = {
+        query: query,
+        location: 'US',
+    };
+
+    try {
+        const [rows] = await bigqueryClient.query(options);
+        return rows;
+    } catch (error) {
+        console.error('Error running query:', error);
+        return [];
+    }
+};
+
+///THREAD
+// Latest threads
+async function getLatestThreads(){
+    const query = 'SELECT * FROM `sciplay_dataset.thread` ORDER BY date_time DESC LIMIT 10;';
+    const options = {
+        query: query,
+        location: 'US',
+    };
+
+    const [rows] = await bigqueryClient.query(options);
+    for (let i = 0; i < rows.length; i++)
+    {
+        const time = rows[i].date_time.value.slice(0, 10);
+        rows[i].time = time;
+        rows[i].title = rows[i].content.slice(0, 40) + '...';
+    }
+    return rows;
+}
+// Specific thread
+async function getThread(thread_id){
+    const [row] = await getDataFromBigQuery(bigqueryClient, {
+        datasetId: 'sciplay_dataset',
+        tableId: 'thread',
+        column: ['*'],
+        conditions: [`thread_id = ${thread_id}`],
+        limit: 1,
+    });
+    console.log(row.date_time.value.toString().slice('T'));
+    const [date, time] = row.date_time.value.slice('-');
+    row.time = time;
+    row.date = date;
+
+    return row;
+}
+// Thread comments
+async function getThreadComment(thread_id){
+    const rows = await getDataFromBigQuery(bigqueryClient, {
+        datasetId: 'sciplay_dataset',
+        tableId: 'thread_comment',
+        column: ['*'],
+        conditions: [`thread_id = ${thread_id}`],
+        limit: 20,
+    });
+
+    return rows;
+}
+
+async function Test () {
+    const stu = await getStudentLessons('3');
+    console.log(stu);
+}
+Test();
 
 module.exports = {
     getStudent,
@@ -124,5 +221,9 @@ module.exports = {
     getLesson, 
     getStudentCourses,
     getStudentLessons,
-    getFinishedCourses
+    getFinishedCourses,
+    getRanking, 
+    getLatestThreads,
+    getThread,
+    getThreadComment,
 }
