b03df408fcb08301b26664c4d224eb1ee7d0b93e
Do-Trang <thutrang25062k4@gmail.com>
2023-08-04 15:59:42 +0700

    UI frontend login & page

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from './assets/logoname.png'
+import Logo from '../../assets/logoname.png'
 import './Login.css'
 function Login({ history }) {
 
