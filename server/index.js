78fc3086451208bdb19906f197f049b9cfad09f6
np4s <npanh1104@gmail.com>
2023-08-04 21:27:55 +0700

    refactor: uncomment listen

--- server/index.js
+++ server/index.js
@@ -161,6 +161,6 @@ app.get('/api/thread', async (req, res) => {
 
 
 
-// app.listen(port, () => {
-//     console.log(`Server is running on http://localhost:${port}`);
-//   });
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
