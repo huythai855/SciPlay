920fca5ef640964c5adc4ae7aa981c9dbfb40a3f
unknown <maintn124@gmail.com>
2023-08-04 16:02:42 +0700

    ui: add navigation

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, /*Link*/ } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Lesson from './pages/Lesson/Lesson';
 import Login from './pages/Login/Login';
+import Learn from "./pages/Learn";
+import Rankings from "./pages/Rankings";
+import Challenges from "./pages/Challenges";
+import Forum from "./pages/Forum";
+import Shop from "./pages/Shop";
 
 function AppRouter() {
   return (
@@ -11,7 +16,11 @@ function AppRouter() {
         <Routes>
           <Route path="/" exact element={<Home/>} />
           <Route path="/login" element={<Login/>} />
-          {/* <Route path="/homepage" element={<HomePage />} /> */}
+          <Route path="/learn" element={<Learn/>} />
+          <Route path="/rankings" element={<Rankings/>} />
+          <Route path="/challenges" element={<Challenges/>} />
+          <Route path="/forum" element={<Forum/>} />
+          <Route path="/shop" element={<Shop/>} />
           <Route path="/lesson/:id" element={<Lesson />} />
 
           {/* 404 page if you guy wanna edit :> */}
