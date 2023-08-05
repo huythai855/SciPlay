929be1d5fc06749e8d1d78ec1fbb1aa3486c63bb
np4s <npanh1104@gmail.com>
2023-08-05 15:01:27 +0700

    feat: connect server to recommend

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ const bodyParser = require('body-parser');
 const { BigQuery } = require('@google-cloud/bigquery');
 const { Storage } = require('@google-cloud/storage');
 const cors = require('cors');
-const { getStudent, getCourse, getLesson, getStudentCourses, getStudentLessons, getFinishedCourses, getRanking, getLatestThreads, getThread, getThreadComment, getInsideCourse, getTopicLesson } = require('./helper');
+const { 
+    getStudent, getCourse, getLesson, getStudentCourses, getStudentLessons, 
+    getFinishedCourses, getRanking, getLatestThreads, getThread, getThreadComment, 
+    getInsideCourse, getTopicLesson } = require('./helper');
+const { getRecommendation } = require('./rec_from_some')
 
 const app = express();
 const port = 3000;
@@ -45,11 +49,13 @@ app.get('/api/course', async (req, res) => {
         if (course_id === undefined) {
             const student = await getStudent(user_id);
             const courses = await getStudentCourses(user_id);
+            const recommend = await getRecommendation(user_id, 3);
             const data = {
                 student,
                 courses,
+                recommend
             }
-            // console.log(data);
+            // console.log(data)
             res.json(data);
         } else {
             if (course_path_id === undefined) {
